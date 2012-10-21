2890266cb196b1afd2f59632c8fd50a5efa19134
sane.vova@gmail.com <sane.vova@gmail.com@528f9c44-9ce6-e12a-c302-0bba194b370b>
2012-10-21 17:40:41 +0000

    Added task08_StringSplitting and task09_PrimeNumber solutions.

---
+++ trunk/mishatkin_vladimir/task09_PrimeNumber/PrimeNumber.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <assert.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int n) {
+    if (n == 2)
+        return true;
+    if (n == 1 || n % 2 == 0)
+        return false;
+    for (int i = 3; i * i < n; i += 2)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+int main() {
+    int N;
+    scanf("%d\n", &N);
+    if (isPrime(N))
+        printf("YES\n");
+    else
+        printf("NO\n");
+    return 0;
+}
