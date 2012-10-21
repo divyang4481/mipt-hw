ad78552f88b9ae2483f43a2a914c1374ad30485b
sane.vova@gmail.com <sane.vova@gmail.com@528f9c44-9ce6-e12a-c302-0bba194b370b>
2012-10-21 16:29:29 +0000

    some commit message.

---
+++ mishatkin_vladimir/task07_Comparison/includes.h
@@ -0,0 +1,12 @@
+#ifndef __includes_h_included
+#define __includes_h_included
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <assert.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+#endif //__includes_h_included
