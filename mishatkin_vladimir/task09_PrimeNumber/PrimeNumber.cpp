26ddb5e5a537bfa95bbd3932570e3d2d3a87c73a
sane.vova@gmail.com <sane.vova@gmail.com@528f9c44-9ce6-e12a-c302-0bba194b370b>
2012-10-28 18:06:24 +0000

    Fixed PrimeNumber.

--- mishatkin_vladimir/task09_PrimeNumber/PrimeNumber.cpp
+++ mishatkin_vladimir/task09_PrimeNumber/PrimeNumber.cpp
@@ -11,7 +11,7 @@ bool isPrime(int n) {
         return true;
     if (n == 1 || n % 2 == 0)
         return false;
-    for (int i = 3; i * i < n; i += 2)
+    for (int i = 3; i * i <= n; i += 2)
         if (n % i == 0)
             return false;
     return true;
