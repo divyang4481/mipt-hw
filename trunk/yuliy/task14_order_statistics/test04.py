30fdc85c151cc33cb8355577d51bfc5683a910d1
aivyulik@gmail.com <aivyulik@gmail.com@528f9c44-9ce6-e12a-c302-0bba194b370b>
2012-11-26 06:25:49 +0000

    task14 tests

---
+++ trunk/yuliy/task14_order_statistics/test04.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+
+n = 10 * 1000000
+k = 5 * 1000000
+data = []
+
+def generate():
+    for _ in xrange(0, k):
+        data.append(5)
+    for _ in xrange(0, n-k):
+        data.append(7)
+
+def main():
+    print n, k
+    generate()
+    for elem in data:
+        print elem
+
+    data.sort()
+    print>>sys.stderr, ''
+    print>>sys.stderr, ('test04 answer=%d' % data[k])
+
+if __name__ == '__main__':
+    main()
